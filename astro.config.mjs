407ccd6dd4285214d5dbaa176d6e50b1d31e2d3a
Pablo Portas López <81629707+TeenBiscuits@users.noreply.github.com>
2025-06-07 20:46:34 +0200

    Revert "Remove @astrojs/tailwind because it is deprecated"
    This reverts commit a2fdaf0bdfcd8ec1e61317330acaf4b9ca5cbbe4.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 import icon from "astro-icon";
@@ -9,5 +10,5 @@ export default defineConfig({
     "/blog": "/",
   },
   site: "https://www.pablopl.dev",
-  integrations: [sitemap(), icon()],
+  integrations: [tailwind(), sitemap(), icon()],
 });
